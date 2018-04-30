4e93855a1e26097f361e11913f8ca716ba9ca8cf
Stefania Pedrazzi <stefania.pedrazzi@cyberbotics.com>
2018-04-30 16:45:16 +0200

    Send record request only in case of win and if host.txt exists

--- controllers/supervisor/record_result.h
+++ controllers/supervisor/record_result.h
@@ -4,11 +4,14 @@
 
 
 static inline void notify_game_result(const size_t *team_id, const std::array<std::size_t, 2> &score) {
-  /*
+  if (score[0] <= score[1])
+    // no win: nothing to do
+    return;
+
   char host[1024];
   FILE *file = fopen("../../host.txt", "r");
   if (!file) {
-    fprintf(stderr, "Error: cannot open host file.\n");
+    // simulation not executed on the streaming server
     return;
   }
   size_t n = fscanf(file, "%1023s", host);
@@ -22,15 +25,12 @@ static inline void notify_game_result(const size_t *team_id, const std::array<st
   strncpy(fqdn, &host[start], sizeof(fqdn));
   char *colon = strchr(fqdn, ':');
   if (colon)
-    *colon = '\0';*/
-  // TODO compute automatically?
-  char fqdn[1024] = "localhost";
-  char host[1024] = "localhost";
+    *colon = '\0';
   const char *WEBOTS_HOME = getenv("WEBOTS_HOME");
-  size_t n = strlen(WEBOTS_HOME) + 1024;
+  n = strlen(WEBOTS_HOME) + 1024;
   char *filename = (char *)malloc(n);
   snprintf(filename, n, "%s/resources/web/server/key/%s", WEBOTS_HOME, fqdn);
-  FILE *file = fopen(filename, "r");
+  file = fopen(filename, "r");
   if (!file) {
     fprintf(stderr, "Error: cannot open key file '%s'\n", filename);
     free(filename);
@@ -47,14 +47,13 @@ static inline void notify_game_result(const size_t *team_id, const std::array<st
       break;
     }
   }
-  const int win = (score[0] > score[1]) + (score[0] < score[1]) * 2;
   const int l = 1024;
   char *command = (char *)malloc(l);
   snprintf(command, 1024,
            "wget -qO- "
-           "--post-data=\"team1=%zu&team2=%zu&win=%d&key=%s\" "
+           "--post-data=\"user1Id=%zu&user2Id=%zu&key=%s\" "
            "%s/record.php",
-          team_id[0], team_id[1], win, key, host);
+          team_id[0], team_id[1], key, host);
   file = popen(command, "r");
   if (!file) {
     fprintf(stderr, "Error: cannot run wget.\n");
