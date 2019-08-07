90a1c96bb32fe71fb4cf48eccd08cbd972e6ba73
David Mansolino <David.Mansolino@cyberbotics.com>
2019-08-07 17:45:02 +0200

    Rename to try understand...

--- controllers/supervisor/image_frame_buffer.py
+++ controllers/supervisor/image_frame_buffer.py
@@ -19,19 +19,23 @@ class ImageFrameBuffer:
         if self.currentImage is not None:
             self.oldImage = self.currentImage.copy()
         self.currentImage = self.camera.getImageArray()
-        for iy in range(self.subImageWidth):
-            for ix in range(self.subImageHeight):
-                bx = int(ix * (self.width / self.subImageWidth))  # begin x
-                by = int(iy * (self.height / self.subImageHeight))  # begin y
-                ex = self.width if ix == self.subImageWidth - 1 else int(bx + (self.width / self.subImageWidth))
-                ey = self.height if iy == self.subImageHeight - 1 else int(by + (self.height / self.subImageHeight))
+        xDiv = int(self.width / self.subImageWidth)
+        yDiv = int(self.height / self.subImageHeight)
+        for y in range(self.subImageWidth):
+            for x in range(self.subImageHeight):
+                xStart = x * xDiv
+                yStart = y * yDiv
+                ex = self.width if x == self.subImageWidth - 1 else xStart + xDiv
+                ey = self.height if y == self.subImageHeight - 1 else yStart + yDiv
 
                 # compare
-                for py in range(by, ey):
-                    if self.oldImage is None or (self.oldImage[bx][py] == self.oldImage[ex][py] and self.oldImage[bx][py] == self.currentImage[bx][py]):
+                for py in range(yStart, ey):
+                    if self.oldImage is None or (self.oldImage[xStart][py] == self.oldImage[ex][py] and self.oldImage[xStart][py] == self.currentImage[xStart][py]):
                         b64_encoded = ""
-                        for py in range(by, ey):  # TODO: twice py ?!?
-                            for px in range(bx, ex):
-                                b64_encoded += base64.b64encode(bytes(self.currentImage[px][py])).decode("utf-8") 
-                        ret.append([bx, by, ex - bx, ey - by, b64_encoded])
+                        for py in range(yStart, ey):  # TODO: twice py ?!?
+                            for px in range(xStart, ex):
+                                b64_encoded += base64.b64encode(bytes(self.currentImage[px][py])).decode("utf-8")
+                        ret.append([xStart, yStart, ex - xStart, ey - yStart, b64_encoded])
+        print(ret)
+        ret = []
         return ret
